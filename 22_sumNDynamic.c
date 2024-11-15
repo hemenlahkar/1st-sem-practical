c43c8a0c017a60951bc0486bff2d131829846468
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2024-11-15 19:07:53 +0530

    22nd practical added

---
+++ 22_sumNDynamic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Write a program to find sum of n elements entered by the user.
+To write this program, allocate memory dynamically using malloc()/calloc() functions or new operator*/
+
+int main()
+{
+    int *ptr, n, sum = 0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    ptr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("Element[%d]: ", i);
+        scanf("%d", ptr + i);
+    }
+    for (int i = 0; i < n; i++)
+        sum += ptr[i];
+    printf("\nThe sum of the entered numbers is: %d\n", sum);
+    return 0;
+}
