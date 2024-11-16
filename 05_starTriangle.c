9db4f4f5523df56aa9f495dc02120ad9c8b8903a
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2024-11-17 02:03:29 +0530

    minor bug fix

--- 05_starTriangle.c
+++ 05_starTriangle.c
@@ -10,7 +10,7 @@ int main()
         {
             printf(" ");
         }
-        for (int k = 0; k < 2 * i - 1; k++)
+        for (int k = 0; k < 2 * i + 1; k++)
         {
             printf("*");
         }
